8c251c508a39fea3b6d5f6f2fbb4ac0b78a782ab
unu <wnsdn822@cau.ac.kr>
2026-05-26 23:19:02 +0900

    issuefilter annotate

--- src/main/java/issuetracker/model/IssueFilter.java
+++ src/main/java/issuetracker/model/IssueFilter.java
@@ -2,6 +2,18 @@ package issuetracker.model;
 
 import issuetracker.domain.issue.IssueStatus;
 
+/*
+이슈 검색 조건을 하나로 묶는 클래스
+조건마다 메서드 따로 만들필요없이 어떤조건의 조합이던 하나의 메서드로 가능
+ex]
+IssueFilter filter = new IssueFilter()
+    .status(IssueStatus.NEW)
+    .assigneeId(2L)
+    .keyword("로그인");
+
+searchService.searchByFilter(filter);
+*/
+
 public class IssueFilter {
     public IssueStatus status;
     public Long assigneeId;
